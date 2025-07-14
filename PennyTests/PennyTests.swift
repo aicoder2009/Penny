3e5f6c350dfaa0d954c86c6e2eeb506f06c87467
“Karthick <karthickarun2009@gmail.com>
2025-07-14 11:46:21 -0700

    Initial Commit

---
+++ PennyTests/PennyTests.swift
@@ -0,0 +1,16 @@
+//
+//  PennyTests.swift
+//  PennyTests
+//
+//  Created by Karthick Kilash Arun on 7/14/25.
+//
+
+import Testing
+
+struct PennyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
