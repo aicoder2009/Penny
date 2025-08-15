9124b26145a3c2a5bddc9296b2c03f3b7c3bbad4
Karthick Arun <karthickarun2009@gmail.com>
2025-08-14 19:38:42 -0700

    Add files via upload

---
+++ PennyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennyApp.swift
+//  Penny
+//
+//  Created by Karthick Kilash Arun on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct PennyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
